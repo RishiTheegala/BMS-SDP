0d2accab417c5f3c2fe8e557a2f2cad64f17a4cb
Rishi Theegala <73372807+RishiTheegala@users.noreply.github.com>
2025-12-15 18:09:39 -0800

    Change rx_buffers to extern in packet.h

--- Core/Inc/packet.h
+++ Core/Inc/packet.h
@@ -4,7 +4,7 @@
 #include "stm32f3xx_hal_uart.h"
 #include "util.h"
 
-uint8_t rx_buffers[NUM_DEVICES][256];
+extern uint8_t rx_buffers[NUM_DEVICES][256];
 
 void Packet_Init(UART_HandleTypeDef huart);
 void SendCommandPacket(uint8_t cmd, uint8_t *data, int length, uint16_t reg, uint8_t device);
