3b12f93654faf4555a9b28890a52444ee9071778
Rishi <rtheegal@uci.edu>
2026-05-11 13:59:45 -0700

    Got rid of rx_buffer, now get data directly from DMA buffer.

--- Core/Src/UART.c
+++ Core/Src/UART.c
@@ -5,8 +5,7 @@
 #include "stm32f3xx_hal_uart.h"
 #include "stm32f3xx_hal_uart_ex.h"
 
-#define RX_BUFFER_SIZE 256
-#define DMA_RX_BUFFER_SIZE 64
+#define DMA_RX_BUFFER_SIZE 512
 
 typedef struct{
     UART_HandleTypeDef *huart;
@@ -14,34 +13,21 @@ typedef struct{
 
 static UART_Data uartData;
 
-static volatile uint8_t rx_buffer[RX_BUFFER_SIZE];
-static uint8_t dma_rx_buffer[DMA_RX_BUFFER_SIZE];
-static volatile uint16_t dma_last_pos = 0;
-static volatile uint16_t read_index = 0;
-static volatile uint16_t write_index = 0;
+/* DMA receive buffer placed in CCMRAM for deterministic, fast access by DMA */
+static volatile uint8_t dma_rx_buffer[DMA_RX_BUFFER_SIZE] __attribute__((section(".ccmram"))) __attribute__((aligned(4)));
 
-static void UART_PushByte(uint8_t byte)
-{
-    uint16_t next_write = (write_index + 1U) % RX_BUFFER_SIZE;
-
-    if (next_write == read_index)
-    {
-        /* Drop oldest byte when software ring buffer is full. */
-        read_index = (read_index + 1U) % RX_BUFFER_SIZE;
-    }
-
-    rx_buffer[write_index] = byte;
-    write_index = next_write;
-}
+/* dma_write_pos: index (0..DMA_RX_BUFFER_SIZE-1) where DMA has written up to (exclusive)
+   dma_read_pos: index where application will read next */
+static volatile uint16_t dma_write_pos = 0;
+static volatile uint16_t dma_read_pos = 0;
 
 void UART_Init(UART_HandleTypeDef *huart) {
     uartData.huart = huart;
-    read_index = 0;
-    write_index = 0;
-    dma_last_pos = 0;
+    dma_read_pos = 0;
+    dma_write_pos = 0;
 
     if ((uartData.huart != NULL) &&
-        (HAL_UARTEx_ReceiveToIdle_DMA(uartData.huart, dma_rx_buffer, DMA_RX_BUFFER_SIZE) == HAL_OK) &&
+        (HAL_UARTEx_ReceiveToIdle_DMA(uartData.huart, (uint8_t *)dma_rx_buffer, DMA_RX_BUFFER_SIZE) == HAL_OK) &&
         (uartData.huart->hdmarx != NULL))
     {
         __HAL_DMA_DISABLE_IT(uartData.huart->hdmarx, DMA_IT_HT);
@@ -53,33 +39,15 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
     if ((uartData.huart != NULL) && (huart->Instance == uartData.huart->Instance))
     {
         uint16_t pos = Size;
-        uint16_t i;
 
+        /* Size is the current write position in the DMA circular buffer. Update
+           dma_write_pos so application can read directly from dma_rx_buffer. */
         if (pos <= DMA_RX_BUFFER_SIZE)
         {
-            if (pos >= dma_last_pos)
+            dma_write_pos = pos;
+            if (dma_write_pos == DMA_RX_BUFFER_SIZE)
             {
-                for (i = dma_last_pos; i < pos; i++)
-                {
-                    UART_PushByte(dma_rx_buffer[i]);
-                }
-            }
-            else
-            {
-                for (i = dma_last_pos; i < DMA_RX_BUFFER_SIZE; i++)
-                {
-                    UART_PushByte(dma_rx_buffer[i]);
-                }
-                for (i = 0; i < pos; i++)
-                {
-                    UART_PushByte(dma_rx_buffer[i]);
-                }
-            }
-
-            dma_last_pos = pos;
-            if (dma_last_pos == DMA_RX_BUFFER_SIZE)
-            {
-                dma_last_pos = 0;
+                dma_write_pos = 0;
             }
         }
     }
@@ -89,8 +57,9 @@ void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
 {
     if ((uartData.huart != NULL) && (huart->Instance == uartData.huart->Instance))
     {
-        dma_last_pos = 0;
-        if ((HAL_UARTEx_ReceiveToIdle_DMA(uartData.huart, dma_rx_buffer, DMA_RX_BUFFER_SIZE) == HAL_OK) &&
+        dma_read_pos = 0;
+        dma_write_pos = 0;
+        if ((HAL_UARTEx_ReceiveToIdle_DMA(uartData.huart, (uint8_t *)dma_rx_buffer, DMA_RX_BUFFER_SIZE) == HAL_OK) &&
             (uartData.huart->hdmarx != NULL))
         {
             __HAL_DMA_DISABLE_IT(uartData.huart->hdmarx, DMA_IT_HT);
@@ -99,22 +68,23 @@ void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
 }
 
 int UART_ClearRX(void) {
-    read_index = 0;
-    write_index = 0;
+    dma_read_pos = dma_write_pos;
     return 0;
 }
 
 uint8_t UART_GetByte(void) {
-    if (read_index == write_index) {
+    if (dma_read_pos == dma_write_pos) {
         return 0x0;
     }
 
-    uint8_t resp = rx_buffer[read_index];
-    read_index = (read_index + 1U) % RX_BUFFER_SIZE;
+    uint8_t resp = dma_rx_buffer[dma_read_pos];
+    dma_read_pos = (uint16_t)((dma_read_pos + 1U) % DMA_RX_BUFFER_SIZE);
     return resp;
 }
 
 int UART_GetBufferSize(void) {
-    if (write_index >= read_index) return write_index - read_index;
-    return RX_BUFFER_SIZE - (read_index - write_index);
+    uint16_t w = dma_write_pos;
+    uint16_t r = dma_read_pos;
+    if (w >= r) return (int)(w - r);
+    return (int)(DMA_RX_BUFFER_SIZE - (r - w));
 }
